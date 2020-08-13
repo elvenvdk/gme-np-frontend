938df6dd191535ae27999e5dde24fc6481df3b0b
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-08-12 21:55:21 -0400

    Added styles, fonts

---
+++ src/components/common/nav/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Nav .scss';
+
+const Nav = () => {
+  return <div className='nav'>Nav</div>;
+};
+
+export default Nav;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import Nav from '../common/nav/Nav';
+
+import './Layout.scss';
+
 export default function Layout() {
-  return <div>main layout...</div>;
+  return (
+    <div>
+      <Nav />
+      <h1>Layout</h1>
+    </div>
+  );
 }
