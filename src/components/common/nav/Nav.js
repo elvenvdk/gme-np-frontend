4ab96999619a7d88e923aec4636b8512a63d043a
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-08-12 22:19:19 -0400

    Modified nav

--- src/components/common/nav/Nav.js
+++ src/components/common/nav/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Nav .scss';
 
 const Nav = () => {
-  return <div className='nav'>Nav</div>;
+  return <div className='nav'>Registration</div>;
 };
 
 export default Nav;
