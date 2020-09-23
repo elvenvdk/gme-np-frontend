5c7a7a13461db7743e917e0e0cf051d1cb094dc1
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-09-23 00:30:24 -0400

    Added new api methods to frontend

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,24 +1,65 @@
 import axios from 'axios';
 
-const API_AUTH_URL = process.env.REACT_APP_API_AUTH_URL;
+const API_URL = process.env.REACT_APP_API_AUTH_URL;
 
-export const register = async (inputData) => {
+/**
+ *
+ * @param {*} param {}
+ */
+export const register = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+  role,
+  org,
+}) => {
   try {
-    const res = await axios.post(`${API_AUTH_URL}/register`, inputData);
-    console.log(res);
-    return res;
+    const res = await axios.post(`${API_URL}/auth/register`, {
+      firstName,
+      lastName,
+      email,
+      password,
+      role,
+      org,
+    });
+    console.log({ registerationResponse: res.data });
+    return res.data;
   } catch (error) {
     return error.response.data;
   }
 };
 
-export const login = async (inputData) => {
+export const login = async ({ email, password }) => {
   try {
-    const res = await axios.post(`${API_AUTH_URL}/login`, inputData);
-    console.log(res);
+    const res = await axios.post(`${API_URL}/auth/login`, { email, password });
+    console.log({ loginResponse: res.data });
+    return res.data;
   } catch (error) {
     return error.response.data;
   }
 };
 
-export default { register };
+export const emailVerificationCheck = async ({ token }) => {
+  try {
+    const res = await axios.post(`${API_URL}/auth/email-verification`, {
+      token,
+    });
+    console.log({ emailVerificationCheck_Response: res.data });
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const forgotPassword = async ({ email }) => {
+  try {
+    const res = await axios.post(`${API_URL}/forgot-password-verification`, {
+      email,
+    });
+    console.log({ forgotPassword_Response: res.data });
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
