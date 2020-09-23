eba719ea68fc4077a1853686123f263a6a6e8a0e
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-09-22 22:19:44 -0400

    Added ownerRoute and privateRoute

---
+++ src/components/auth/privateRoute/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useGlobal } from 'reactn';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const [userId] = useGlobal('userId');
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        userId ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/auth/signin', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
